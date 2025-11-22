edee03c088bf30d6d2588441c8048361d7f0f7a8
Domusgpt <Phillips.paul.email@gmail.com>
2025-11-22 17:59:00 -0500

    Stabilize visualizer swapping and audio reactive HUD

--- src/visualizers/faceted/FacetedSystem.js
+++ src/visualizers/faceted/FacetedSystem.js
@@ -30,7 +30,49 @@ export class FacetedSystem extends BaseSystem {
         console.log('🎨 Creating Faceted visualizer...');
 
         // Initialize parameter manager
-        this.parameters = new ParameterManager();
+        this.parameters = new ParameterManager({
+            geometry: 0,
+            gridDensity: 15,
+            morphFactor: 1.0,
+            chaos: 0.2,
+            speed: 1.0,
+            hue: 200,
+            intensity: 0.5,
+            saturation: 0.8,
+            dimension: 3.5,
+            rot4dXW: 0.0,
+            rot4dYW: 0.0,
+            rot4dZW: 0.0,
+            colorStyle: 0,
+            colorProfile: 0,
+            colorVibrance: 1.0
+        });
+
+        this.controlBus.attachParameterManager(this.parameters);
+        this.controlBus.defineChannels({
+            colorVibrance: {
+                value: 1.0,
+                range: [0.3, 2.5],
+                smoothing: 0.82,
+                audioMap: [
+                    { path: 'colorChoreography.saturationPulse', scale: 0.4 },
+                    { path: 'extremeDynamics.dimensionLift', scale: 0.25 }
+                ],
+                tags: ['color']
+            },
+            colorStyle: {
+                value: 0,
+                range: [0, 1],
+                smoothing: 0.95,
+                tags: ['color']
+            },
+            colorProfile: {
+                value: 0,
+                range: [0, 9],
+                smoothing: 0.95,
+                tags: ['color']
+            }
+        });
 
         // Create visualizer
         this.visualizer = new IntegratedHolographicVisualizer(
@@ -67,6 +109,11 @@ export class FacetedSystem extends BaseSystem {
             this.visualizer.setParameters(parameters);
         }
 
+        const activeAudio = this.audioEnabled ? audioData : null;
+        if (this.visualizer.setAudioData) {
+            this.visualizer.setAudioData(activeAudio);
+        }
+
         // Get color from color system
         const time = this.visualizer.getTime();
         const color = this.colorSystem.getColor(
@@ -74,7 +121,7 @@ export class FacetedSystem extends BaseSystem {
             this.visualizer.mouseY,
             time,
             parameters.hue || 200,
-            audioData
+            activeAudio
         );
 
         // Update color in visualizer
